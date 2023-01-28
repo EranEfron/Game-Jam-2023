11f7af530d4433b7f6d6858ed11f78ae90a3a657
EranEfron <eranefron@hotmail.com>
2023-01-28 12:30:49 -0600

    temp sprite and movement added

---
+++ KinematicBody2D.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+var speed : int = 200
+var jumpForce : int = 600
+var gravity : int = 800
+var vel : Vector2 = Vector2()
+var grounded : bool = false
+
+onready var sprite = $Sprite
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _physics_process (delta):
+	vel.x = 0
+# movement inputs
+	if Input.is_action_pressed("move_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("move_right"):
+		vel.x += speed
+	vel = move_and_slide(vel, Vector2.UP)
+#	vel.y += gravity * delta
+# jump input
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
