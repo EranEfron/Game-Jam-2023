0f1d707f497e3c95926c15015cb146a3a7ca76cc
EranEfron <eranefron@hotmail.com>
2023-01-29 14:48:53 -0600

    Finished assigning sprites

--- Goal.gd
+++ Goal.gd
@@ -8,13 +8,14 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # movement code (maybe some seeking)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func _on_Area2D_body_entered(body):
-	if body.name == "Player": # Replace with function body.
-		body.die()
+
+func _on_Goal_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene("res://WinScreen.tscn") # Replace with function body.
--- Powerup.gd
+++ Powerup.gd
@@ -1,16 +1,22 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
+# onload causes random number to be generated
 # Called when the node enters the scene tree for the first time.
+onready var ANI = get_node("AnimationPlayer")
+
 func _ready():
-	resize() # Replace with function body.
-	
-func resize():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Powerup_body_entered(body):
+	pass # trigger powerup call in body
